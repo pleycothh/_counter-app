584915d3222861b9bc91e4ffb5a4a5516176a554
tia <ben.li@griffithuni.edu.au>
2022-01-10 22:06:49 +1000

    finsih basic render

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'; // imrc cc
+import App from '../App';
 
 class Counter extends Component {
     state = {
-        count:0,
+        countOne:0,
+        countTwo:0,
+        tag: ['tag1', 'tag2']
     }; // object incldue any data
 
     styles = {
@@ -12,24 +15,75 @@ class Counter extends Component {
 
     render() { 
         return (
+            // element of virtural DOM which are Div, span, button. compare and modify the differetn element
             <div>
+                <span style={ this.styles} className={this.getBadgeClasses()}>{this.formateCountOne()}</span>
+                <button 
+                onClick={() => this.handelIncrement({id:1})} 
+                className='btn btn-secondary btn-sm'
+                >Increment</button>
                 
-                <span style={ this.styles} className={this.getBadgeClasses()}>{this.formateCount()}</span>
-                <button className='btn btn-secondary btn-sm'>Increment</button>
+                <span style={ this.styles} className={this.getBadgeClasses()}>{this.formateCountTwo()}</span>
+                <button 
+                onClick={() => this.handelIncrement({id:2})} 
+                className='btn btn-secondary btn-sm'
+                >Increment</button>
+               
             </div>
+
+            /**
+             *   {this.state.tag.length === 0 && 'Please create a new tag'}
+             *   { this.renderTags() }
+             */
         ); 
+    }
+
+//    constructor(){
+//        super()
+//        this.handelIncrement = this.handelIncrement.bind(this);
+        /**
+         * call constructor from super
+         * constrctor has access to counter obejct
+         * 1. binding will create new handelIncrement instance
+         * handelIncrement function this will always refer to current obejct
+         * 2. reset the handelIncrement with bind method
+         */
+//    }
+
+//    doHandelIncrement = () => {
+//        this.handelIncrement({id:1});
+//    }
+
+    handelIncrement = product => {    // use arrow function, inhertance this key word from super
+        console.log('Increment Clicked', product.id)
+        // this.state.count++    <= will not update the view, react not pass this value
+        if (product.id === 1) this.setState({ countOne: this.state.countOne + 1});
+        if (product.id === 2) this.setState({ countTwo: this.state.countTwo + 1});      
+          // <= embed method from Component method, bring the DOM from virtural to isntance
+          // <= get current state count and increase by one, set count property to this value
+    }
+
+    renderTags(){
+        if ( this.state.tag.length === 0) return <p>There are not tags!</p>;
+        return  <ul>{this.state.tag.map(tag => <li key={tag}>{tag}</li>)}</ul>
     }
 
     getBadgeClasses() {
         let classes = "badge m-2 bg-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
+        classes += (this.state.countOne === 0) ? "warning" : "primary";
         return classes;
     }
 
-    formateCount(){ 
-        const { count } = this.state; // pick count propoty
+    formateCountOne(){ 
+        const { countOne } = this.state; // pick count propoty
         const x = <h1>Zero</h1>;
-        return count === 0 ? x: count;
+        return countOne === 0 ? x: countOne;
+    }
+
+    formateCountTwo(){ 
+        const { countTwo } = this.state; // pick count propoty
+        const x = <h1>Zero</h1>;
+        return countTwo === 0 ? x: countTwo;
     }
 }
  
